055ed7bbb82fc0cb76a1121e5de7697ce3507161
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-15 19:22:06 -0300

    chore(infra): add create users migration

---
+++ src/infra/database/typeorm/migrations/1629057416659-CreateUsers.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1629057416659 implements MigrationInterface {
+  private readonly TABLE_NAME = 'users';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    return queryRunner.createTable(
+      new Table({
+        name: this.TABLE_NAME,
+        columns: [
+          {
+            isPrimary: true,
+            name: 'id',
+            type: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password_hash',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return queryRunner.dropTable(this.TABLE_NAME);
+  }
+}
