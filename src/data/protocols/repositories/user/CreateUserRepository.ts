d70b31bc036e93bd92b6f87ab50475a997123a01
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-15 18:10:35 -0300

    chore(data): add create user repository protocol

---
+++ src/data/protocols/repositories/user/CreateUserRepository.ts
@@ -0,0 +1,11 @@
+import { IUserModel } from '../../../../domain/models/User';
+
+export type CreateUserDTO = {
+  name: string;
+  email: string;
+  password_hash: string;
+};
+
+export interface ICreateUserRepository {
+  create(data: CreateUserDTO): Promise<IUserModel>;
+}
