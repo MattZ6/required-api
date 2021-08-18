0eb13ae81a1b6966b72ce4f0ae01b6de3f347bc0
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-18 19:53:12 -0300

    chore: add make authenticate user controller factory

---
+++ src/main/factories/controllers/authentication/AuthenticateUserControllerFactory/index.ts
@@ -0,0 +1,22 @@
+import { AuthenticateUserUseCase } from '../../../../../data/usecases/authenticate-user/AuthenticateUser';
+import { JWTCriptographyProvider } from '../../../../../infra/criptography/criptography/JWTCriptographyProvider';
+import { BcryptHashProvider } from '../../../../../infra/criptography/hash/BcryptHashProvider';
+import PostgresUsersRepository from '../../../../../infra/database/typeorm/repositories/postgres/PostgresUsersRepository';
+import { AuthenticateUserController } from '../../../../../presentation/controllers/authentication/AuthenticateUserController';
+import { IController } from '../../../../../presentation/protocols/Controller';
+
+export const makeAuthenticateUserController = (): IController => {
+  const bcryptHashProvider = new BcryptHashProvider(12);
+  const jwtCriptographyProvider = new JWTCriptographyProvider(
+    process.env.AUTH_SECRET,
+    '15m'
+  );
+
+  const authenticateUserUseCase = new AuthenticateUserUseCase(
+    PostgresUsersRepository,
+    bcryptHashProvider,
+    jwtCriptographyProvider
+  );
+
+  return new AuthenticateUserController(authenticateUserUseCase);
+};
