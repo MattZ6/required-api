222c4151f7eb9dcfe56ba44d8dfe8655a0167b49
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-18 19:49:45 -0300

    chore: add ok and not found http helpers

--- src/presentation/helpers/http/http.ts
+++ src/presentation/helpers/http/http.ts
@@ -1,9 +1,23 @@
 import { IHttpRespose } from '../../protocols/Http';
 
+export function ok<T = any>(data: T): IHttpRespose<T> {
+  return {
+    statusCode: 200,
+    body: data,
+  };
+}
+
 export function created<T = any>(data?: T): IHttpRespose<T> {
   return {
     statusCode: 201,
     body: data,
+  };
+}
+
+export function notFound(error: Error): IHttpRespose<Error> {
+  return {
+    statusCode: 404,
+    body: error,
   };
 }
 
