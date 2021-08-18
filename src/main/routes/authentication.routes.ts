5f7baa8bdb460da4e3742c326ebe303990a54138
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-18 19:53:42 -0300

    chore(routes): add login route

--- src/main/routes/authentication.routes.ts
+++ src/main/routes/authentication.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { adaptRoute } from '../adapters/express/express-route-adapter';
+import { makeAuthenticateUserController } from '../factories/controllers/authentication/AuthenticateUserControllerFactory';
 import { makeCreateAccountController } from '../factories/controllers/authentication/CreateAccountControllerFactory';
 
 const authenticationRoutes = Router();
@@ -8,6 +9,11 @@ const authenticationRoutes = Router();
 authenticationRoutes.post(
   '/account',
   adaptRoute(makeCreateAccountController())
+);
+
+authenticationRoutes.post(
+  '/login',
+  adaptRoute(makeAuthenticateUserController())
 );
 
 export default authenticationRoutes;
