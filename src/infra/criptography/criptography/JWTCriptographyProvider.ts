248c6d70f94407dea22d0377a317694d39188b56
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-18 19:48:28 -0300

    chore(criptography): add jwt criptography provider implementation

---
+++ src/infra/criptography/criptography/JWTCriptographyProvider.ts
@@ -0,0 +1,17 @@
+import { sign } from 'jsonwebtoken';
+
+import { IEncryptProvider } from '../../../data/protocols/cryptography/criptography/EncryptProvider';
+
+export class JWTCriptographyProvider implements IEncryptProvider {
+  constructor(
+    private readonly secret: string,
+    private readonly expiresIn: string | number
+  ) {}
+
+  async encrypt(value: string): Promise<string> {
+    return sign({}, this.secret, {
+      subject: value,
+      expiresIn: this.expiresIn,
+    });
+  }
+}
