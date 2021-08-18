3ab54e1c304ba8bcdf4832e6dabd55a9c2366aa5
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-18 19:39:50 -0300

    chore(BcryptHashProvider): add compare hash implementation

--- src/infra/criptography/hash/BcryptHashProvider.ts
+++ src/infra/criptography/hash/BcryptHashProvider.ts
@@ -1,11 +1,16 @@
-import { hash } from 'bcryptjs';
+import { hash, compare } from 'bcryptjs';
 
+import { ICompareHash } from '../../../data/protocols/cryptography/hash/CompareHash';
 import { ICreateHash } from '../../../data/protocols/cryptography/hash/CreateHash';
 
-export class BcryptHashProvider implements ICreateHash {
+export class BcryptHashProvider implements ICreateHash, ICompareHash {
   constructor(private readonly salt: number) {}
 
   hash(value: string): Promise<string> {
     return hash(value, this.salt);
+  }
+
+  compare(value: string, hashedValue: string): Promise<boolean> {
+    return compare(value, hashedValue);
   }
 }
