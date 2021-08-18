f231d3e270b2bdbdab9f6d885ff4b2b78da74f30
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-18 19:38:52 -0300

    chore(PostgresUsersRepository): add find user by email implementation

--- src/infra/database/typeorm/repositories/postgres/PostgresUsersRepository.ts
+++ src/infra/database/typeorm/repositories/postgres/PostgresUsersRepository.ts
@@ -5,11 +5,15 @@ import {
   CreateUserDTO,
   ICreateUserRepository,
 } from '../../../../../data/protocols/repositories/user/CreateUserRepository';
+import { IFindUserByEmailRepository } from '../../../../../data/protocols/repositories/user/FindUserByEmailRepository';
 import { IUserModel } from '../../../../../domain/models/User';
 import { User } from '../../entities/User';
 
 class PostgresUsersRepository
-  implements ICheckIfUserExistsByEmail, ICreateUserRepository
+  implements
+    ICheckIfUserExistsByEmail,
+    ICreateUserRepository,
+    IFindUserByEmailRepository
 {
   private repository: Repository<User>;
 
@@ -29,12 +33,22 @@ class PostgresUsersRepository
     return count >= 1;
   }
 
-  create(data: CreateUserDTO): Promise<IUserModel> {
+  async create(data: CreateUserDTO): Promise<IUserModel> {
     const { name, email, password_hash } = data;
 
     const user = this.repository.create({ name, email, password_hash });
 
     return this.repository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<IUserModel | undefined> {
+    return this.repository.findOne({
+      where: {
+        email: Raw(field => `LOWER(${field}) = LOWER(:value)`, {
+          value: email,
+        }),
+      },
+    });
   }
 }
 
