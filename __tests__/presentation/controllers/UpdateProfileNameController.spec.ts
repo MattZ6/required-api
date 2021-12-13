6a11fd71874e5fcbbb4a42e24237fa860598819b
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-12-13 10:11:18 -0300

    test(UpdateProfileNameController): add unit tests

---
+++ __tests__/presentation/controllers/UpdateProfileNameController.spec.ts
@@ -0,0 +1,75 @@
+import Faker from 'faker';
+
+import { UserNotFoundWithThisIdError } from '@domain/errors';
+
+import { UpdateProfileNameController } from '@presentation/controllers/profile/UpdateProfileNameController';
+import { noContent, notFound } from '@presentation/helpers/http/http';
+
+import { UpdateUserNameUseCaseSpy } from '../mocks';
+
+let updateUserNameUseCaseSpy: UpdateUserNameUseCaseSpy;
+
+let updateProfileNameController: UpdateProfileNameController;
+
+describe('UpdateProfileNameController', () => {
+  beforeEach(() => {
+    updateUserNameUseCaseSpy = new UpdateUserNameUseCaseSpy();
+
+    updateProfileNameController = new UpdateProfileNameController(
+      updateUserNameUseCaseSpy
+    );
+  });
+
+  it('should call UpdateProfileNameController with correct data', async () => {
+    const executeSpy = jest.spyOn(updateUserNameUseCaseSpy, 'execute');
+
+    const user_id = Faker.datatype.uuid();
+    const name = Faker.name.findName();
+
+    await updateProfileNameController.handle({ user_id, body: { name } });
+
+    expect(executeSpy).toHaveBeenCalledTimes(1);
+    expect(executeSpy).toHaveBeenCalledWith({ user_id, name });
+  });
+
+  it('should throw if UpdateProfileNameController throws', async () => {
+    jest
+      .spyOn(updateUserNameUseCaseSpy, 'execute')
+      .mockRejectedValueOnce(new Error());
+
+    const promise = updateProfileNameController.handle({
+      user_id: Faker.datatype.uuid(),
+      body: { name: Faker.name.findName() },
+    });
+
+    await expect(promise).rejects.toThrow();
+  });
+
+  it('should return 404 if UpdateUserNameUseCase throws UserNotFoundWithThisIdError', async () => {
+    const error = new UserNotFoundWithThisIdError();
+
+    jest
+      .spyOn(updateUserNameUseCaseSpy, 'execute')
+      .mockRejectedValueOnce(error);
+
+    const response = await updateProfileNameController.handle({
+      user_id: Faker.datatype.uuid(),
+      body: {
+        name: Faker.name.findName(),
+      },
+    });
+
+    expect(response).toEqual(notFound(error));
+  });
+
+  it('should return 204 on success', async () => {
+    const response = await updateProfileNameController.handle({
+      user_id: Faker.datatype.uuid(),
+      body: {
+        name: Faker.name.findName(),
+      },
+    });
+
+    expect(response).toEqual(noContent());
+  });
+});
--- __tests__/presentation/mocks/index.ts
+++ __tests__/presentation/mocks/index.ts
@@ -2,3 +2,4 @@ export * from './authenticate-user.usecase';
 export * from './create-user.usecase';
 export * from './get-profile.usecase';
 export * from './update-user-email.usecase';
+export * from './update-user-name.usecase';
