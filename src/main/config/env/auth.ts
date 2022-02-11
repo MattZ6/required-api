0f3dbb51172fb35314ad1688411f3c42868ead49
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-02-11 15:17:02 -0300

    fix(authConfig): jwt auth secret variable name

--- src/main/config/env/auth.ts
+++ src/main/config/env/auth.ts
@@ -1,7 +1,7 @@
 export const authConfig = {
   /** Authentication */
 
-  ACCESS_TOKEN_SECRET: process.env.AUTH_SECRET,
+  ACCESS_TOKEN_SECRET: process.env.JWT_AUTH_SECRET,
   ACCESS_TOKEN_EXPIRES_IN_SECONDS: 15 * 60, // 👈 15 min
 
   /** Hash provider */
