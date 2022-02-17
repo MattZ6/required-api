21d26e930dd4117135cb15412bae2c0ebf6a2408
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-02-17 12:37:07 -0300

    feat(JWTCryptographyProvider): add verify implementation

--- src/infra/providers/cryptography/cryptography/JWTCryptographyProvider.ts
+++ src/infra/providers/cryptography/cryptography/JWTCryptographyProvider.ts
@@ -1,8 +1,24 @@
-import { sign } from 'jsonwebtoken';
+import {
+  JwtPayload,
+  TokenExpiredError,
+  JsonWebTokenError,
+  sign,
+  verify,
+} from 'jsonwebtoken';
 
-import { IEncryptProvider } from '@application/protocols/providers/cryptography/cryptography';
+import {
+  IEncryptProvider,
+  IVerifyCriptographyProvider,
+} from '@application/protocols/providers/cryptography/cryptography';
 
-export class JWTCryptographyProvider implements IEncryptProvider {
+import {
+  AccessTokenExpiredError,
+  InvalidAccessTokenError,
+} from '@presentation/errors';
+
+export class JWTCryptographyProvider
+  implements IEncryptProvider, IVerifyCriptographyProvider
+{
   constructor(
     private readonly secret: string,
     private readonly expiresInSeconds: number
@@ -17,5 +33,27 @@ export class JWTCryptographyProvider implements IEncryptProvider {
       subject: value,
       expiresIn: this.expiresInSeconds,
     });
+  }
+
+  async verify(
+    data: IVerifyCriptographyProvider.Input
+  ): Promise<IVerifyCriptographyProvider.Output> {
+    const { value } = data;
+
+    try {
+      const { sub } = verify(value, this.secret) as JwtPayload;
+
+      return { payload: sub };
+    } catch (error) {
+      if (error instanceof TokenExpiredError) {
+        throw new AccessTokenExpiredError();
+      }
+
+      if (error instanceof JsonWebTokenError) {
+        throw new InvalidAccessTokenError();
+      }
+
+      throw error;
+    }
   }
 }
