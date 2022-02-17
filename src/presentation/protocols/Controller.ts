ed0a2944f7f38625ca8e6e0834760bec99e8c0e3
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-02-17 12:31:58 -0300

    refactor(presentation): change imports on controller interface

--- src/presentation/protocols/Controller.ts
+++ src/presentation/protocols/Controller.ts
@@ -1,4 +1,4 @@
-import { IHttpRequest, IHttpResponse } from './Http';
+import { IHttpRequest, IHttpResponse } from '.';
 
 export interface IController {
   handle(request: IHttpRequest): Promise<IHttpResponse>;
