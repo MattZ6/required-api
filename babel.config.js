85021efb3ed4bbd58b9f0751f029d7cb3b621a84
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-05-25 20:39:29 -0300

    build: add babel for build application

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript"
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@domain": "./src/domain",
+          "@application": "./src/application",
+          "@infra": "./src/infra",
+          "@presentation": "./src/presentation",
+          "@main": "./src/main"
+        }
+      }
+    ],
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }]
+  ]
+}
