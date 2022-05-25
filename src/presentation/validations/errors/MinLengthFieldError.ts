4d649fa813e1e77d6590d513764a3e748db01567
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-05-25 13:47:14 -0300

    refactor(MinLengthFieldError): add value

--- src/presentation/validations/errors/MinLengthFieldError.ts
+++ src/presentation/validations/errors/MinLengthFieldError.ts
@@ -4,8 +4,9 @@ export class MinLengthFieldError extends ValidationError {
   constructor(fieldName: string, minLength: number) {
     const message = `The ${fieldName} must have at least ${minLength} characters`;
 
-    super(fieldName, 'minlength', message);
+    super(fieldName, 'minlength', message, minLength);
     super.message = message;
     super.field = fieldName;
+    super.value = minLength;
   }
 }
