139a4a13b8d20cf44e1b1edc1f9188889d889945
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-05-26 15:30:15 -0300

    docs(RefreshToken): add documentation

--- src/domain/errors/user/token/UserTokenNotFoundWithProvidedTokenError.ts
+++ src/domain/errors/user/token/UserTokenNotFoundWithProvidedTokenError.ts
@@ -1,7 +1,7 @@
 import { DomainError } from '@domain/errors';
 
 export class UserTokenNotFoundWithProvidedTokenError extends DomainError {
-  constructor(message = 'Token not present.', code = 'token.not.provided') {
+  constructor(message = 'Token not found', code = 'user_token.not.exists') {
     super(message, code);
   }
 }
