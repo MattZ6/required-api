19c5cc51752e5fcef96df8576eef797bb6166a38
Matheus Felipe Zanin <matt_z6@hotmail.com>
2023-06-20 17:04:18 -0300

    chore: update health check script

--- src/main/config/healthCheck.ts
+++ src/main/config/healthCheck.ts
@@ -1,7 +1,12 @@
+import { execSync } from 'child_process'
 import type { Express } from 'express'
 
 export function setupHealthCheckEndpoint(app: Express) {
   // Maybe run migrations at this time
 
-  app.get('/health', (_, res) => res.status(200).send())
+  app.get('/health', (_, res) => {
+    execSync(`npm run prisma migrate deploy`, { stdio: 'inherit' })
+
+    return res.status(200).send()
+  })
 }
